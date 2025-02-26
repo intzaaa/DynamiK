4b2bcd6a964e704d5be58a02b71c5a80af726138
intzaaa <huangyinhaow@outlook.com>
2025-02-26 11:09:35 +0800

    fix lang tag

--- src/utils/i18n.tsx
+++ src/utils/i18n.tsx
@@ -1,3 +1,5 @@
+import { effect, signal } from "@preact/signals";
+
 // https://stackoverflow.com/questions/58434389/typescript-deep-keyof-of-a-nested-object/58436959
 export type Paths<T> = T extends object
   ? {
@@ -14,36 +16,42 @@ export type Leaves<T> = T extends object
 const deep_value = <T extends object>(o: T, path: string) => path.split(".").reduce((a, v) => (a as any)[v], o);
 
 export default function setup<T extends object, L extends string>(fallback_lang: L, data: T) {
+  const selected = signal<string>(fallback_lang);
+
+  effect(() => {
+    document.documentElement.lang = selected.value;
+  });
+
   const database: {
     [key: string]: T;
   } = {
     [fallback_lang]: data,
   };
 
+  const match = (langs: readonly string[], fallback: string, dbLangs: string[]): string => {
+    if (!langs.length) return fallback;
+    const [lang, region] = langs[0]!.split("-");
+    const filtered = dbLangs.filter((val) => val.startsWith(lang!));
+    if (filtered.length) {
+      const exact = filtered.find((val) => {
+        const [, database_region] = val.split("-");
+        return database_region === region;
+      });
+      return exact ?? filtered.sort((a, b) => a.length - b.length)[0]!;
+    }
+    return match(langs.slice(1), fallback, dbLangs);
+  };
+
   const register = (lang: string, data: T) => {
     database[lang] = data;
+
+    const user_langs = navigator.languages;
+    const database_langs = Object.keys(database);
+    selected.value = match(user_langs, fallback_lang, database_langs);
   };
 
   const Text = ({ path }: { path: Leaves<T> }) => {
-    const user_langs = navigator.languages;
-    const database_langs = Object.keys(database);
-
-    const match = (langs: readonly string[], fallback: string, dbLangs: string[]): string => {
-      if (!langs.length) return fallback;
-      const [lang, region] = langs[0]!.split("-");
-      const filtered = dbLangs.filter((val) => val.startsWith(lang!));
-      if (filtered.length) {
-        const exact = filtered.find((val) => {
-          const [, database_region] = val.split("-");
-          return database_region === region;
-        });
-        return exact ?? filtered.sort((a, b) => a.length - b.length)[0]!;
-      }
-      return match(langs.slice(1), fallback, dbLangs);
-    };
-
-    const selected = match(user_langs, fallback_lang, database_langs);
-    const value = deep_value(database[selected]!, path);
+    const value = deep_value(database[selected.value]!, path);
     return Object.assign(<>{value}</>, { raw: value as unknown as string });
   };
 
