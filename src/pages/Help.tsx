fc2ca1e65f09084fc69c2d99b8afabafd9ba8d41
intzaaa <huangyinhaow@outlook.com>
2025-02-27 22:59:32 +0800

    fix dark mode help

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -4,7 +4,7 @@ import { Text } from "..";
 export default function Help() {
   return (
     <>
-      <div class="w-full h-0 grow overflow-y-scroll overflow-x-clip prose p-4 dark:text-white">
+      <div class="w-full h-0 grow overflow-y-scroll overflow-x-clip prose p-4 dark:prose-invert">
         <p>Build {__BUILD_TIME__}</p>
         <Text path="helpContent" />
       </div>
